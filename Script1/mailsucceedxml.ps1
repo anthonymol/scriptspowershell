cf4a9729e8b0b057e43cd6554eb8c3dc01118477
anthonymol <80119522+anthonymol@users.noreply.github.com>
2021-03-05 18:41:38 +0100

    Create mailsucceedxml.ps1

---
+++ Script1/mailsucceedxml.ps1
@@ -0,0 +1,55 @@
+﻿#CredObj path
+$CredObj = ("C:\Users\a.mollier\Desktop\script\Préprod\Email.cred")
+
+ 
+
+#Check if CredObj is present
+$CredObjCheck = Test-Path -Path $CredObj
+If (!($CredObjCheck))
+{
+    "$Date - INFO: creating cred object"
+    
+    #If not present get office 365 cred to save and store
+    $Credential = Get-Credential -Message "Please enter your Office 365 credential that you will use to send e-mail from $FromEmail. If you are not using the account $FromEmail make sure this account has 'Send As' rights on $FromEmail."
+    
+    #Export cred obj
+    $Credential | Export-CliXml -Path $CredObj
+}
+
+Write-Host "Importing Cred object..." -ForegroundColor Yellow
+$Cred = (Import-CliXml -Path $CredObj)
+
+$SmtpClient = new-object system.net.mail.smtpClient
+$MailMessage = New-Object system.net.mail.mailmessage
+        
+#Sender
+$mailmessage.From = "a.mollier@axido.fr"
+#SMTP server to send email
+$SmtpClient.Host = "smtp.office365.com"
+$SmtpClient.Port = 587
+#SMTP SSL
+$SMTPClient.EnableSsl = $true
+#SMTP credentials
+$SMTPClient.Credentials = $cred
+#Send e-mail to the users email
+$mailmessage.To.add("a.mollier@axido.fr")
+#Email subject
+$mailmessage.Subject = "CHECK FILES XML"
+#Send e-mail with high priority
+$MailMessage.Priority = "High"
+$mailmessage.Body = "The copy of the XML files SUCCEED"
+
+ 
+
+Write-Host "Sending E-mail to $emailaddress" -ForegroundColor Green
+
+ 
+
+Try
+{
+    $smtpclient.Send($mailmessage)
+}
+Catch
+{
+    $_ 
+}
